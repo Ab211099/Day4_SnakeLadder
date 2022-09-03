3516f55744f25cc2b49947e02398b595bb4ec861
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-03 15:20:55 +0530

    [Abhi]Added : Start_Postion

---
+++ snakeLadder/UC1_StartPostiotn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeLadder
+{
+     public class UC1_StartPostiotn
+    {
+        public void startPosition()
+        {
+             int START_POSITION = 0;
+
+            Console.WriteLine("Welcome to snake ladder game");
+            Console.WriteLine("Player will start from zero position");
+        }
+    }
+}
