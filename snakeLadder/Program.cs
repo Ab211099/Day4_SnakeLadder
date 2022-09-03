af750b25302bf501711afcacbc564309380f1dcc
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-03 15:18:48 +0530

    [Abhi]Added; Main method

---
+++ snakeLadder/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace snakeLadder
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Enter the Program number \n 1.Player start the position");
+
+            int optionCheck = Convert.ToInt32(Console.ReadLine());
+
+            switch (optionCheck)
+            {
+                case 1:
+                    UC1_StartPostiotn obj = new UC1_StartPostiotn();    
+                    obj.startPosition();
+                    break;
+            }
+        }
+    }
+}
