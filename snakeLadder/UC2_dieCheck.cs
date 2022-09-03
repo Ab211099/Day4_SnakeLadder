c2ac6e33c9cc0ed7ead545d46e54fb834f669003
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-03 15:30:29 +0530

    [Abhi]Added: Die_Check program

---
+++ snakeLadder/UC2_dieCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeLadder
+{
+    internal class UC2_dieCheck
+    {
+        public void dieCheck()
+        {
+            int START_POSITION = 0;
+
+            Console.WriteLine("Welcome to snake ladder game");
+            Console.WriteLine("Player will start from zero position");
+
+            //Die Check
+            Random random = new Random();
+            int dieCheck = random.Next(0, 7);
+            if (dieCheck == 0)
+            {
+                dieCheck = dieCheck + 1;
+
+            }
+            Console.WriteLine("Number on die is: " + dieCheck);
+        }
+    }
+}
